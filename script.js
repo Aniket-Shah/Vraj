7af9d6dd58e4aa0a164f6d10d94b7b01baa528df
Aniket Shah <aniket.shah@impactguru.net>
2025-06-12 16:40:28 +0530

    About Us and Categories

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var crsr = document.querySelector("#cursor")
-document.addEventListener("mousemove", function (dets) {
-    crsr.style.left = dets.x-5 + "px";
-    crsr.style.top = dets.y-1 + "px";
-});
+// var crsr = document.querySelector("#cursor")
+// document.addEventListener("mousemove", function (dets) {
+//     crsr.style.left = dets.x-5 + "px";
+//     crsr.style.top = dets.y-1 + "px";
+// });
