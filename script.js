df651b07ea0e54c98c37fbd617b3cb9a184b7e20
Aniket Shah <aniket.shah@impactguru.net>
2025-06-12 16:35:25 +0530

    About Us and Categories

--- script.js
+++ script.js
@@ -3,21 +3,3 @@ document.addEventListener("mousemove", function (dets) {
     crsr.style.left = dets.x-5 + "px";
     crsr.style.top = dets.y-1 + "px";
 });
-
-function loadHTML(id, file) {
-  fetch(file)
-    .then((res) => {
-      if (!res.ok) throw new Error(`Failed to load ${file}`);
-      return res.text();
-    })
-    .then((html) => {
-      document.getElementById(id).innerHTML = html;
-    })
-    .catch((err) => console.error(err));
-}
-
-loadHTML("include-nav", "sections/nav.html");
-loadHTML("include-hero", "sections/hero.html");
-loadHTML("include-service", "sections/service.html");
-loadHTML("include-about", "sections/about.html");
-loadHTML("include-category", "sections/category.html");
